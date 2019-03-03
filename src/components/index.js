130e1bea9e56fe1e2d9259729a540a1e56cade02
Fredrik Gustafsson <Fredrik.Gustafsson@nordnet.se>
2019-03-03 16:59:01 +0100

    Clean up components index file

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,5 @@ import App from "./App";
 
 export { default as Table } from "./Table";
 export { default as ClothesParser } from "./ClothesParser";
-export { default as JsonParser } from "./JsonParser";
-export { default as XmlParser } from "./XmlParser";
-export { default as CsvParser } from "./CsvParser";
 
 export default App;
